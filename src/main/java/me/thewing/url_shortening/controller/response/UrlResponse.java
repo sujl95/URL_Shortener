2ca33bdfdcfbc59e3ac8704fdabbaec32da69370
sujl95 <sujl95@naver.com>
2021-04-11 01:16:25 +0900

    UrlResponse, index.html Fix Test Create

--- src/main/java/me/thewing/url_shortening/controller/response/UrlResponse.java
+++ src/main/java/me/thewing/url_shortening/controller/response/UrlResponse.java
@@ -1,11 +1,10 @@
 package me.thewing.url_shortening.controller.response;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class UrlResponse {
-	private final String url;
+	private final String ShortUrl;
 }
---
+++ src/test/java/me/thewing/url_shortening/ShortUrlTest.java
@@ -0,0 +1,61 @@
+package me.thewing.url_shortening;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import me.thewing.url_shortening.dto.UrlCreateDto;
+
+@DisplayName("Short Url 통합 Test")
+@SpringBootTest
+@AutoConfigureMockMvc
+class ShortUrlTest {
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@Autowired
+	ObjectMapper objectMapper;
+
+	@Test
+	@DisplayName("ShortUrl 생성 테스트")
+	void createShortUrlTest() throws Exception{
+		String originUrl = "https://sujl95.tistory.com/";
+		UrlCreateDto urlCreateDto = UrlCreateDto.builder()
+				.url(originUrl)
+				.build();
+		mockMvc.perform(post("/")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(urlCreateDto)))
+				.andDo(print())
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("shortUrl").value("thewing.cf/cXk1OFA4"));
+	}
+
+	@Test
+	@DisplayName("ShortUrl logs 조회 테스트 - 성공")
+	void findByShortUrl() throws Exception {
+		String shortUrl = "cXk1OFA4";
+		mockMvc.perform(get("/logs/{shortUrl}", shortUrl))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	@DisplayName("ShortUrl logs 조회 테스트 - 실패")
+	void findByShortUrlFailure() throws Exception {
+		String shortUrl = "asdfasdf";
+		mockMvc.perform(get("/logs/{shortUrl}", shortUrl))
+				.andExpect(status().isNotFound());
+	}
+
+}
